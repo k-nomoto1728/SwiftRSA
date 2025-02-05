026f8519189794eafabcb73bd2f5d1a62010f4e8
knomoto <nomotokeiichiro@gmail.com>
2025-02-05 22:55:01 +0900

    Initial Commit

---
+++ RSA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RSA
+//
+//  Created by Nomoto  Keiichiro on 2025/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RSA/RSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSAApp.swift
+//  RSA
+//
+//  Created by Nomoto  Keiichiro on 2025/02/05.
+//
+
+import SwiftUI
+
+@main
+struct RSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
